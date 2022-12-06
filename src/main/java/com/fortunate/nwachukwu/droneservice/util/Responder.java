e40f3481ef550c5755ee7ada5633ee4d3ef06f3d
NfoTECH <fortunenwachukwu@gmail.com>
2022-12-06 01:50:50 +0100

    modify the Responder add custom response messages

--- src/main/java/com/fortunate/nwachukwu/droneservice/util/Responder.java
+++ src/main/java/com/fortunate/nwachukwu/droneservice/util/Responder.java
@@ -1,5 +1,6 @@
 package com.fortunate.nwachukwu.droneservice.util;
 
+import com.fortunate.nwachukwu.droneservice.exception.*;
 import com.fortunate.nwachukwu.droneservice.payload.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,22 +10,40 @@ import static org.springframework.http.HttpStatus.*;
 public class Responder {
 
     public static ResponseEntity<ApiResponse> success(Object response){
-        return new ResponseEntity<>(new ApiResponse("Registered successfully", true, response), OK);
+        return new ResponseEntity<>(new ApiResponse("Successful", true, response), OK);
     }
 
     public static ResponseEntity<ApiResponse> found (Object response) {
         return new ResponseEntity<>(new ApiResponse("Retrieved successfully", true, response), FOUND);
     }
 
-    public static ResponseEntity<ApiResponse> conflict (Object response) {
-        return new ResponseEntity<>(new ApiResponse("Already exist", false), CONFLICT);
-    }
+//    public static ResponseEntity<ApiResponse> conflict (Object response) {
+//        return new ResponseEntity<>(new ApiResponse("Already exist", false), CONFLICT);
+//    }
+public static ResponseEntity<ApiResponse> conflict (Object response) {
+    return new ResponseEntity<>(new ApiResponse(((DroneAlreadyRegisteredException)response).getMessage(), false), CONFLICT);
+}
 
     public static ResponseEntity<ApiResponse> notFound (Object response) {
-        return new ResponseEntity<>(new ApiResponse("Not found", false), NOT_FOUND);
+        return new ResponseEntity<>(new ApiResponse(((NoAvailableDroneException)response).getMessage(), false), NOT_FOUND);
     }
 
-    public static ResponseEntity<ApiResponse> notAllowed (Object response) {
-        return new ResponseEntity<>(new ApiResponse<>("Not allowed", false), FORBIDDEN);
+//    public static ResponseEntity<ApiResponse> overWeight (Object response) {
+//        return new ResponseEntity<>(new ApiResponse<>("Weight Limit exceeded", false), FORBIDDEN);
+//    }
+    public static ResponseEntity<ApiResponse> overWeight (Object response) {
+        return new ResponseEntity<>(new ApiResponse<>(((WeightLimitExceededException)response).getMessage(), false), FORBIDDEN);
+    }
+//
+//    public static ResponseEntity<ApiResponse> lowBattery (Object response) {
+//        return new ResponseEntity<>(new ApiResponse<>("Drone battery too low", false), FORBIDDEN);
+//    }
+
+    public static ResponseEntity<ApiResponse> lowBattery (Object response) {
+        return new ResponseEntity<>(new ApiResponse<>(((DroneBatteryLowException)response).getMessage(), false), FORBIDDEN);
+    }
+
+    public static ResponseEntity<ApiResponse> badRequest (Object response) {
+        return new ResponseEntity<>(new ApiResponse<>(((InvalidLoadRequestException)response).getMessage(), false), BAD_REQUEST);
     }
 }
