1a544d203c658bb73f8901f7f4529e5b7998e2c1
NfoTECH <fortunenwachukwu@gmail.com>
2022-12-06 16:02:08 +0100

    modify DroneResponse add fromDelivery method

--- src/main/java/com/fortunate/nwachukwu/droneservice/payload/response/DroneResponse.java
+++ src/main/java/com/fortunate/nwachukwu/droneservice/payload/response/DroneResponse.java
@@ -1,2 +1,23 @@
-package com.fortunate.nwachukwu.droneservice.payload.response;public class DroneResponse {
+package com.fortunate.nwachukwu.droneservice.payload.response;
+
+import com.fortunate.nwachukwu.droneservice.model.Drone;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DroneResponse {
+    private String serialNumber;
+    private Double batteryLevel;
+
+    public static  DroneResponse fromDrone(Drone drone) {
+        return DroneResponse.builder()
+                .serialNumber(drone.getSerialNumber())
+                .batteryLevel(drone.getBatteryCapacity())
+                .build();
+    }
 }
