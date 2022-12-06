5c0915c6e915de45939ab4a52ddb93ee6622c7b4
NfoTECH <fortunenwachukwu@gmail.com>
2022-12-06 23:44:48 +0100

    clean up the medication repository to remove redundancies

--- src/main/java/com/fortunate/nwachukwu/droneservice/repository/MedicationRepository.java
+++ src/main/java/com/fortunate/nwachukwu/droneservice/repository/MedicationRepository.java
@@ -1,18 +1,11 @@
 package com.fortunate.nwachukwu.droneservice.repository;
 
 import com.fortunate.nwachukwu.droneservice.model.Medication;
-import com.fortunate.nwachukwu.droneservice.payload.response.ApiResponse;
-import com.fortunate.nwachukwu.droneservice.payload.response.MedicationResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 public interface MedicationRepository extends JpaRepository<Medication, Long> {
-    Optional<Medication> findByCode(String medicineCode);
-    Optional<Medication> findByName(String medicineName);
-
-
     List<Medication> findAllByCodeIn(Set<String> codes);
 }
