23957c5e9b3c5051f49150aad44208ae6aaa293a
NfoTECH <fortunenwachukwu@gmail.com>
2022-12-06 16:15:03 +0100

    modeify the DroneServiceApplication add the @EnableScheduling annotation

--- src/main/java/com/fortunate/nwachukwu/droneservice/DroneServiceApplication.java
+++ src/main/java/com/fortunate/nwachukwu/droneservice/DroneServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,6 +19,7 @@ import java.util.List;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class DroneServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DroneServiceApplication.class, args);
