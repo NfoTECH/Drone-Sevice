8c59b4279d395fbf8b3413c9efaf5b4bdbe6dc63
NfoTECH <fortunenwachukwu@gmail.com>
2022-12-06 15:48:12 +0100

    modify the Medication model remove the drone field

--- src/main/java/com/fortunate/nwachukwu/droneservice/model/Medication.java
+++ src/main/java/com/fortunate/nwachukwu/droneservice/model/Medication.java
@@ -3,10 +3,7 @@ package com.fortunate.nwachukwu.droneservice.model;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Entity
@@ -25,7 +22,4 @@ public class Medication extends BaseModel {
     private String code;
 
     private String image;
-    @JsonBackReference
-    @ManyToOne
-    private Drone drone;
 }
