e0eb8540c00f1b7d52470686cae327b19352fc15
NfoTECH <fortunenwachukwu@gmail.com>
2022-12-06 15:41:18 +0100

    add InvalidLoadRequestException to the exception package

---
+++ src/main/java/com/fortunate/nwachukwu/droneservice/exception/InvalidLoadRequestException.java
@@ -0,0 +1,7 @@
+package com.fortunate.nwachukwu.droneservice.exception;
+
+public class InvalidLoadRequestException extends RuntimeException {
+    public InvalidLoadRequestException(String message) {
+        super(message);
+    }
+}
