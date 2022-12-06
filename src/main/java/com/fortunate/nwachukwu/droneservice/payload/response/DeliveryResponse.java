39a37e6c60b5a6b01f8e150cd311c7a730f915aa
NfoTECH <fortunenwachukwu@gmail.com>
2022-12-06 16:13:52 +0100

    modify the DeliveryResponse add the fromDelivery mdethod

--- src/main/java/com/fortunate/nwachukwu/droneservice/payload/response/DeliveryResponse.java
+++ src/main/java/com/fortunate/nwachukwu/droneservice/payload/response/DeliveryResponse.java
@@ -1,2 +1,31 @@
-package com.fortunate.nwachukwu.droneservice.payload.response;public class DeliveryResponse {
+package com.fortunate.nwachukwu.droneservice.payload.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fortunate.nwachukwu.droneservice.model.Delivery;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class DeliveryResponse {
+    private DroneResponse drone;
+    private List<MedicationResponse> medications;
+    @CreationTimestamp
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss a")
+    private LocalDateTime createAt;
+
+    public static DeliveryResponse fromDelivery(Delivery delivery) {
+        return DeliveryResponse.builder()
+                .drone(DroneResponse.fromDrone(delivery.getDrone()))
+                .medications(delivery.getMedications().stream().map(MedicationResponse::fromMedication).collect(Collectors.toList()))
+                .createAt(delivery.getCreatedAt())
+                .build();
+    }
 }
