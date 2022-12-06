de4bd78fe3ab97707efd6f99e1cc817a240c902a
NfoTECH <fortunenwachukwu@gmail.com>
2022-12-06 15:38:28 +0100

    add LocalDateConverter and DroneBatterPeriodicCheck to the util class

--- src/main/java/com/fortunate/nwachukwu/droneservice/util/Responder.java
+++ src/main/java/com/fortunate/nwachukwu/droneservice/util/Responder.java
@@ -3,7 +3,6 @@ package com.fortunate.nwachukwu.droneservice.util;
 import com.fortunate.nwachukwu.droneservice.exception.*;
 import com.fortunate.nwachukwu.droneservice.payload.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -17,9 +16,6 @@ public class Responder {
         return new ResponseEntity<>(new ApiResponse("Retrieved successfully", true, response), FOUND);
     }
 
-//    public static ResponseEntity<ApiResponse> conflict (Object response) {
-//        return new ResponseEntity<>(new ApiResponse("Already exist", false), CONFLICT);
-//    }
 public static ResponseEntity<ApiResponse> conflict (Object response) {
     return new ResponseEntity<>(new ApiResponse(((DroneAlreadyRegisteredException)response).getMessage(), false), CONFLICT);
 }
@@ -28,16 +24,9 @@ public static ResponseEntity<ApiResponse> conflict (Object response) {
         return new ResponseEntity<>(new ApiResponse(((NoAvailableDroneException)response).getMessage(), false), NOT_FOUND);
     }
 
-//    public static ResponseEntity<ApiResponse> overWeight (Object response) {
-//        return new ResponseEntity<>(new ApiResponse<>("Weight Limit exceeded", false), FORBIDDEN);
-//    }
     public static ResponseEntity<ApiResponse> overWeight (Object response) {
         return new ResponseEntity<>(new ApiResponse<>(((WeightLimitExceededException)response).getMessage(), false), FORBIDDEN);
     }
-//
-//    public static ResponseEntity<ApiResponse> lowBattery (Object response) {
-//        return new ResponseEntity<>(new ApiResponse<>("Drone battery too low", false), FORBIDDEN);
-//    }
 
     public static ResponseEntity<ApiResponse> lowBattery (Object response) {
         return new ResponseEntity<>(new ApiResponse<>(((DroneBatteryLowException)response).getMessage(), false), FORBIDDEN);
