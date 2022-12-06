7bcf47768957fcc144ac9ae9b717b9f8ec12e799
NfoTECH <fortunenwachukwu@gmail.com>
2022-12-06 15:45:51 +0100

    add the Delivery model

--- src/main/java/com/fortunate/nwachukwu/droneservice/model/Delivery.java
+++ src/main/java/com/fortunate/nwachukwu/droneservice/model/Delivery.java
@@ -1,2 +1,22 @@
-package com.fortunate.nwachukwu.droneservice.model;public class Delivery {
+package com.fortunate.nwachukwu.droneservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+@Entity
+@Table(name = "deliveries")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Delivery extends BaseModel {
+    @OneToOne
+    private Drone drone;
+    @OneToMany
+    private List<Medication> medications;
+    private Boolean isDelivered = false;
 }
