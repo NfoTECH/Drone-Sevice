ef20fa080c1e8acd77ecc8cc01b58a10de72562c
NfoTECH <fortunenwachukwu@gmail.com>
2022-12-07 15:40:07 +0100

    add @Builder annotation for easy loadADroneWithMedicationWhenDroneIsAvailable test

--- src/main/java/com/fortunate/nwachukwu/droneservice/payload/request/LoadingRequest.java
+++ src/main/java/com/fortunate/nwachukwu/droneservice/payload/request/LoadingRequest.java
@@ -1,6 +1,7 @@
 package com.fortunate.nwachukwu.droneservice.payload.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class LoadingRequest {
     Set<String> medicationCodes;
 }
